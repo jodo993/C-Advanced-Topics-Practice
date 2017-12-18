8d593e0a9f19c64fdf2b5d1253d32c430e5fb982
jodo993 <josephdo993@yahoo.com>
2017-12-17 21:33:46 -0800

    Delegates
    Has info about the function and not data
    - revisit later

---
+++ Filters.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C_Sharp___Delegates
+{
+    class Filters
+    {
+        public void ApplyBrightness(Photo photo)
+        {
+            Console.WriteLine("Done bright.");
+        }
+
+        public void ApplyContrast(Photo photo)
+        {
+            Console.WriteLine("Done contrast.");
+        }
+        public void Resize(Photo photo)
+        {
+
+            Console.WriteLine("Done size.");
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C_Sharp___Delegates
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var photo = new PhotoProcess();
+            var filters = new Filters();
+            PhotoProcess.PhotoFilterHandler filterHandler = filters.ApplyBrightness;
+            filterHandler += filters.ApplyContrast;
+            filterHandler += RemoveRedEyes;
+
+            photo.Process("photo.jpg", filterHandler);
+        }
+
+        static void RemoveRedEyes(Photo photo)
+        {
+            Console.WriteLine("Apply RemoveRedEye");
+        }
+    }
+}
